91570ffd0f7a46b8a6ddbc35699b6fdca70d18b1
Patricia Gonzalez <info.patriciagonzalez@gmail.com>
2021-07-15 15:12:43 -0300

    jvs2

---
+++ ejercicio2javascript/js/recetario.js
@@ -0,0 +1,5 @@
+var nombre = prompt ('Cuál es tu nombre? :')
+alert ('Hola! Bienvenid@ : ' +nombre)
+var ingrediente1 = prompt ('Ingresa el primer ingrediente:');
+var ingrediente2 = prompt ('Ingresa el segundo ingrediente:');
+var ingrediente3 = prompt ('Ingresa el último ingrediente:');
