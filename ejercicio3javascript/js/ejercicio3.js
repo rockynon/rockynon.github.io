07352c0e5d97a7752cd24c29fdf4e4dd9fd095e0
Patricia Gonzalez <info.patriciagonzalez@gmail.com>
2021-07-27 19:52:46 -0300

    final

--- ejercicio3javascript/js/ejercicio3.js
+++ ejercicio3javascript/js/ejercicio3.js
@@ -1,5 +1,5 @@
 var nombre = prompt ('Cuál es tu nombre? :')
-alert ('Hola! Bienvenid@ : ' + nombre + ', ingresa los números para ver el resultado de las diferentes operaciones matemáticas. ')
+alert ('Hola! Bienvenido : ' + nombre + ', ingresa los números para ver el resultado de las diferentes operaciones matemáticas. ')
 var num1 = Number (prompt ('Primer número: ' ));
 var num2 = Number (prompt ('Segundo número: '));
 var sum =  num1 + num2;
